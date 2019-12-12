fe151fa48702267ee7b98a5d5b9b516a5f9d449f
Laptop 0 <programming@farmingtonrobotics.org>
2019-12-12 16:49:51 -0500

    update

---
+++ Vanden/Vanden.ino
@@ -0,0 +1,17 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 2   // input pin Neopixel is attached to
+
+#define NUMPIXELS      16 // number of neopixels in strip
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+  int i=0;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
